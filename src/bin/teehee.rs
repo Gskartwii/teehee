0e1acc2b24166c7458e1376e99ab2a54ea429a37
Aleksi Hannula <ahannula4@gmail.com>
2020-08-16 11:42:24 +0300

    Allow starting the program without opening a file

--- src/bin/teehee.rs
+++ src/bin/teehee.rs
@@ -6,8 +6,10 @@ const STDOUT_BUF: usize = 8192;
 fn main() {
     let stdout = stdout();
     let mut stdout = BufWriter::with_capacity(STDOUT_BUF, stdout.lock());
-    let filename = std::env::args().nth(1).expect("Need a filename");
-    let contents = std::fs::read(&filename).expect("Couldn't read file");
+    let filename = std::env::args().nth(1);
+    let contents = filename
+        .map(|filename| std::fs::read(&filename).expect("Couldn't read file"))
+        .unwrap_or(vec![]);
     let view = HexView::from_data(contents);
 
     view.run_event_loop(&mut stdout).unwrap();
